3a367a65019988c85e6c9c0c1057aa1fb8fa999b
furenku <furenku@gmail.com>
2018-09-10 20:46:37 -0500

    parsing input string in all levels

--- frontend/src/SongCapture.js
+++ frontend/src/SongCapture.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SongTextInput from './SongTextInput';
 import SongTextDisplay from './SongTextDisplay';
 
+const syllableSeparationCharacter = "·";
+
 class SongCapture extends React.Component {
     
     state = {
@@ -19,6 +21,9 @@ class SongCapture extends React.Component {
                 value={this.state.inputString}
                 onChange={ (e) => this.handleChange(e) }
                 />
+
+                { this.drawSongText( this.parseSongText( this.state.inputString ) ) }
+
                 <SongTextDisplay/>
 
                 <footer className="capture_player">
@@ -37,6 +42,77 @@ class SongCapture extends React.Component {
         });
 
     }
+
+
+    parseSongText( inputString ) {
+
+        let stanzas = this.parseStanzas ( inputString );
+        
+        stanzas.forEach( stanza => {
+            let verses = this.parseVerses( stanza )
+            verses.forEach( verse => {
+                let words = this.parseWords( verse )
+
+                words.forEach( word => {
+                    let syllables = this.parseSyllables( word )
+                    
+                    syllables.forEach( syllable => {
+                        console.log( syllable )
+                    });
+        
+                });
+
+                
+            });
+    
+        });
+
+        
+        let result = [];
+
+        return result
+
+    }
+
+    parseStanzas( inputString ) {
+        let result = [];
+
+        let newString = inputString;
+        
+        // 1. limpiar el string de secuencias
+        // con tres o más newLines \n
+        // convertirlas en \n\n, en caso de existir
+
+
+        // 2. separar string usando \n\n
+        
+        result = newString.split("\n\n");
+
+        return result;
+
+    } 
+
+    parseVerses( inputString ) {
+        let result = inputString.split("\n");
+        return result;
+    } 
+
+    parseWords( inputString ) {
+        let result = inputString.split(" ");
+        return result;
+    } 
+
+    parseSyllables( inputString ) {
+        let result = inputString.split( syllableSeparationCharacter );
+        return result;
+    }
+
+    drawSongText( songTextArray ) {
+
+        return <h1>{songTextArray}</h1>
+
+    }
+
 
 }
 
